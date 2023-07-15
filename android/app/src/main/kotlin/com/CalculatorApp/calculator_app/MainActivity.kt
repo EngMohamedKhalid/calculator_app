9975b7e555a2e47240413f2118ae08a3a1e435a4
eng_mohamed_Khalid <engmohamedkhalid4404@gmail.com>
2023-07-16 01:43:30 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/CalculatorApp/calculator_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.CalculatorApp.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/claculator_screen.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  List<String> symbols = [
+    'C',
+    'Del',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    '*',
+    '6',
+    '5',
+    '4',
+    '+',
+    '3',
+    '2',
+    '1',
+    '-',
+    '.',
+    '0',
+    'Ans',
+    '=',
+  ];
+  String input = '';
+  String output = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+          elevation: 0, title: const Text('Calculator'), centerTitle: true),
+      body: Column(
+        children: [
+          const SizedBox(height: 20,),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: const EdgeInsets.all(5),
+              width: double.infinity,
+              child: Text(
+                input,
+                textAlign: TextAlign.left,
+                style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: const EdgeInsets.all(5),
+              width: double.infinity,
+              child: Text(
+                output,
+                textAlign: TextAlign.right,
+                style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.redAccent),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: GridView.builder(
+              itemCount: symbols.length,
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                childAspectRatio: 1.2
+              ),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    if (symbols[index] == 'C') {
+                      setState(() {
+                        input = '';
+                        output = '';
+                      });
+                    } else if (symbols[index] == 'Del') {
+                      setState(() {
+                        input = input.substring(0, input.length - 1);
+                      });
+                    } else if (symbols[index] == 'Ans') {
+                      setState(() {
+                        input = output;
+                        output = '';
+                      });
+                    } else if (symbols[index] == '%' ||
+                        symbols[index] == '/' ||
+                        symbols[index] == '*' ||
+                        symbols[index] == '+' ||
+                        symbols[index] == '-') {
+                      if (input.endsWith("%") ||
+                          input.endsWith("*") ||
+                          input.endsWith("/") ||
+                          input.endsWith("+") ||
+                          input.endsWith("-")) {
+                        //
+                      } else {
+                        setState(() {
+                          input += symbols[index];
+                        });
+                      }
+                    } else if (symbols[index] == '=') {
+                      Expression exp = Parser().parse(input);
+                      double result =
+                      exp.evaluate(EvaluationType.REAL, ContextModel());
+                      setState(() {
+                        output = result.toString();
+                      });
+
+                      //output
+                    } else {
+                      setState(() {
+                        input += symbols[index];
+                      });
+                    }
+                    //logic
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: myBackgroundColor(symbols[index]),
+                        borderRadius: BorderRadius.circular(20)),
+                    alignment: Alignment.center,
+                    child: Text(
+                      symbols[index],
+                      style: TextStyle(
+                        color: myTextColor(symbols[index]),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Color myBackgroundColor(String x) {
+    if (x == 'C') {
+      return Colors.purple;
+    } else if (x == 'Del') {
+      return Colors.pink;
+    }else if(x == '='){
+      return Colors.green;
+    }
+    else if (x == '%' ||
+        x == '/' ||
+        x == '+' ||
+        x == '*' ||
+        x == '-' ) {
+      return Colors.teal;
+    } else {
+      return Colors.grey.shade300;
+    }
+  }
+
+  Color myTextColor(String x) {
+    if (x == 'C' ||
+        x == '%' ||
+        x == 'Del' ||
+        x == '/' ||
+        x == '+' ||
+        x == '*' ||
+        x == '-' ||
+        x == '=') {
+      return Colors.white;
+    } else {
+      return Colors.black;
+    }
+  }
+}
